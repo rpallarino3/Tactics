9cc19a934c06fad832191947c69e7404c312b75f
Ryan <rpallarino3@gmail.com>
2014-09-02 22:53:57 -0400

    added some chat stuff

---
+++ Tactics/Tactics/Game/Chat/MessageBlock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Tactics.Game.Inventory;
+
+namespace Tactics.Game.Chat
+{
+    class MessageBlock
+    {
+        private List<string> messages;
+        private List<List<string>> options;
+        private List<List<int>> destinations;
+        private List<Item> items;
+
+        public MessageBlock()
+        {
+            messages = new List<string>();
+            options = new List<List<string>();
+            destinations = new List<List<int>>();
+            items = new List<Item>();
+        }
+
+        public void giveItem(Bag bag, int index)
+        {
+        }
+
+        public void addItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void addMessage(string message, List<int> destinations, List<string> options)
+        {
+            this.messages.Add(message);
+            this.destinations.Add(destinations);
+            this.options.Add(options);
+        }
+
+        public List<string> getMessages()
+        {
+            return messages;
+        }
+
+        public string getMessage(int index)
+        {
+            return messages[index];
+        }
+
+        public List<List<string>> getOptions()
+        {
+            return options;
+        }
+
+        public List<string> getOptions(int index)
+        {
+            return options[index];
+        }
+
+        public List<List<int>> getDestinations()
+        {
+            return destinations;
+        }
+
+        public List<int> getDestination(int index)
+        {
+            return destinations[index];
+        }
+    }
+}
---
+++ Tactics/Tactics/Game/Chat/MessageBlockFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tactics.Game.Chat
+{
+    class MessageBlockFactory
+    {
+        private List<MessageBlock> objectBlocks;
+        private List<MessageBlock> currentCharacterBlocks;
+
+        public MessageBlockFactory()
+        {
+            objectBlocks = new List<MessageBlock>();
+            currentCharacterBlocks = new List<MessageBlock>();
+
+            createObjectBlocks();
+        }
+
+        public List<MessageBlock> getObjectBlocks()
+        {
+            return objectBlocks;
+        }
+
+        public MessageBlock getObjectBlock(int index)
+        {
+            return objectBlocks[index];
+        }
+
+        private void createObjectBlocks()
+        {
+        }
+
+        public List<MessageBlock> getCurrentCharacterBlocks()
+        {
+            return currentCharacterBlocks;
+        }
+
+        public MessageBlock getCurrentCharacterBlock(int index)
+        {
+            return currentCharacterBlocks[index];
+        }
+
+        public void createRegionCharacterBlocks(int region)
+        {
+            if (region == 0)
+            {
+            }
+        }
+    }
+}
